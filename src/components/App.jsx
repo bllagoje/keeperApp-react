37ae2db0fc20b2356fde2310594f85b297fef8fc
bllagoje <bllagoje@gmail.com>
2022-12-28 20:28:31 +0100

    Add Note function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import CreateArea from "./CreateArea";
 
-import notes from "./notes";
+// import notes from "./notes";
 
 
 function App() {
+    const [notes, setNotes] = useState([]);
+
+
+    function addNote(note) {
+        // console.log(note);
+        setNotes(prevNotes => {
+            return [...prevNotes, note];
+        });
+
+    };
+
+    // ------------------------------------------------------------------------------------------
     return(
         <div>
             <Header />
-            {notes.map(note => 
-                <Note key={note.key} title={note.title} content={note.content} />
-            )}
+            <CreateArea 
+                onAdd={addNote}
+            />
+            {notes.map((noteItem) => {
+            return (
+                <Note 
+                    title={noteItem.title} 
+                    content={noteItem.content}
+                />
+            )
+            })}
+            {/* <Note key={1} title="Note title" content="Note content" /> */}
             <Footer />
         </div>
     );
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    });
+
+    function handleChange(event) {
+        let {name, value} = event.target;
+        setNote(prevNote => {
+            return {
+                ...prevNote,
+                [name]: value
+            }
+        })
+    };
+
+    function submitNote(event) {
+        props.onAdd(note);
+
+        // Prevent Default:
+        event.preventDefault(); 
+    };
+
+// ------------------------------------------------------------------------------------------
+  return (
+    <div>
+      <form>
+        <input 
+            name="title" 
+            placeholder="Title"
+            onChange={handleChange} 
+            value={note.value}
+        />
+        <textarea 
+            name="content" 
+            placeholder="Take a note..." 
+            rows="3"
+            onChange={handleChange}
+            value={note.content}
+        />
+        <button onClick={submitNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
